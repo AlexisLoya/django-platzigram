c520eb08b17c46aee1df0357f397bf8ac0d4432f
AlexisLoya <alexloy117@gmail.com>
2021-01-03 23:32:59 -0600

    response Json

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -18,5 +18,7 @@ from django.urls import path
 from platzigram import views
 
 urlpatterns = [
-    path('hello-world/', views.hello_world)
+    path('hello-world/', views.hello_world),
+    path('hi/', views.hi)
+
 ]
--- platzigram/views.py
+++ platzigram/views.py
@@ -1,5 +1,6 @@
 # Django
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 # Utilities
 from datetime import datetime
@@ -9,3 +10,13 @@ def hello_world(request):
     return HttpResponse('Oh, hi! Current server time is {now}'.format(
         now=datetime.now().strftime('%b %dth, %Y - %H:%M hrs')
     ))
+
+def hi(request):
+    sorted_numbers = sorted(request.GET['numbers'].split(','))
+    data = {
+        'status': 'ok',
+        'numbers': sorted_numbers,
+        'message': 'sorted_numbers successfully.'
+
+    }
+    return JsonResponse(data, safe=False)
