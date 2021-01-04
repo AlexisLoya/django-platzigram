71b434d92f8fa8bf13fd0236136035213984882d
AlexisLoya <alexloy117@gmail.com>
2021-01-03 22:21:58 -0600

    hello world django

---
+++ platzigram/views.py
@@ -0,0 +1,11 @@
+# Django
+from django.http import HttpResponse
+
+# Utilities
+from datetime import datetime
+
+def hello_world(request):
+    """Return a greeting."""
+    return HttpResponse('Oh, hi! Current server time is {now}'.format(
+        now=datetime.now().strftime('%b %dth, %Y - %H:%M hrs')
+    ))
